4b84467f64e4a07d1135ba5c664cd61ecf9ec6e9
Austin Obimbo <obimboausts@gmail.com>
2024-11-03 08:52:38 +0300

    updated email passwords

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -108,7 +108,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'austinobimbo07@gmail.com'
-EMAIL_HOST_PASSWORD = 'gtho omkr lqxb gntb'
+EMAIL_HOST_PASSWORD = 'gkga oitg fwve ojog'
 DEFAULT_FROM_EMAIL = "austinobimbo07@gmail.com"
 
 # Database
