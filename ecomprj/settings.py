28fa262f9368fbd2df91df85dfed3e39865c1790
Manoj S <51531882+manoj-thamizhan@users.noreply.github.com>
2025-01-08 15:38:25 +0530

    Update settings.py

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -22,7 +22,7 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CSRF_TRUSTED_ORIGINS = ['https://lykeonline.com/']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
